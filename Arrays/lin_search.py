991629cffd0d058eca90b79f4802341cab1f0a7b
arjun-saji-p <arjun2020achu@gmail.com>
2026-06-15 12:38:14 +0530

    Create lin_search.py

---
+++ Arrays/lin_search.py
@@ -0,0 +1,17 @@
+# Linear Search
+
+arr = [10, 20, 30, 40, 50]
+
+key = 30
+
+found = False
+
+for num in arr:
+    if num == key:
+        found = True
+        break
+
+if found:
+    print("Element Found")
+else:
+    print("Element Not Found")
