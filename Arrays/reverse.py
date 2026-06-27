f305831133e50f6e9d3d33526a5d4754d04c1e1f
arjun-saji-p <arjun2020achu@gmail.com>
2026-06-27 22:39:57 +0530

    Implement manual reversal of nums list
    Added a manual reversal method for the nums list.

--- Arrays/reverse.py
+++ Arrays/reverse.py
@@ -5,3 +5,13 @@ arr = [1, 2, 3, 4, 5]
 arr.reverse()
 
 print(arr)
+
+
+nums = [1,2,3,4,5]
+
+n = len(nums)
+
+for i in range(n // 2):
+    nums[i], nums[n - i - 1] = nums[n - i - 1], nums[i]
+
+print(nums)
